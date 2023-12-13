f4687670d49fe8e0ad1e37d97cbf23655269a488
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-12-12 22:46:06 -0500

    completed twofer

--- scala/two-fer/src/main/scala/Twofer.scala
+++ scala/two-fer/src/main/scala/Twofer.scala
@@ -1,11 +1,7 @@
 object Twofer {
-  def twofer(name: String): String = {
-    // val nullResponse: String = "One for you, one for me."
-    // val nonNullResponse: String = s"One for $name, one for me"
-    // if (name == Nil) {
-    //   return nullResponse
-    // }
-    // return nonNullResponse
-    return ""
-  }
+
+  def twofer(): String = "One for you, one for me."
+
+  def twofer(name: String): String = s"One for $name, one for me."
+
 }
